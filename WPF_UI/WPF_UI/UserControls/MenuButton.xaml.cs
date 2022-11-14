fe87df6becc1ce9d43bd469c2e874cf93c6f5cc4
YG <YG@DESKTOP-29PU1UU>
2022-11-14 23:33:01 +0900

    UI Update

---
+++ WPF_UI/WPF_UI/UserControls/MenuButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.IconPacks;
+
+namespace WPF_UI.UserControls
+{
+    /// <summary>
+    /// MenuButton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("", typeof(PackIconMaterialKind), typeof(MenuButton));
+
+
+
+
+    }
+}
