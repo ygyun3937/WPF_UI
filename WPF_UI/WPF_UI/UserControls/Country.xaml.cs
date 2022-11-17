42efeca43bd0c7d599f4f583458b873f1c573084
YG <YG@DESKTOP-29PU1UU>
2022-11-17 22:12:29 +0900

    UI 초기화면 완료 업데이트

---
+++ WPF_UI/WPF_UI/UserControls/Country.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF_UI.UserControls
+{
+    /// <summary>
+    /// Country.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Country : UserControl
+    {
+        public Country()
+        {
+            InitializeComponent();
+        }
+        public string CountryName
+        {
+            get { return (string)GetValue(CountryNameProperty); }
+            set { SetValue(CountryNameProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty CountryNameProperty =
+            DependencyProperty.Register("CountryName", typeof(string), typeof(Country));
+
+
+        public string Price
+        {
+            get { return (string)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register("Price", typeof(string), typeof(Country));
+
+
+
+        public bool IsLevelUp
+        {
+            get { return (bool)GetValue(IsLevelUpProperty); }
+            set { SetValue(IsLevelUpProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsLevelUpProperty =
+            DependencyProperty.Register("IsLevelUp", typeof(bool), typeof(Country));
+
+
+
+        public ImageSource Flag
+        {
+            get { return (ImageSource)GetValue(FlagProperty); }
+            set { SetValue(FlagProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty FlagProperty =
+            DependencyProperty.Register("Flag", typeof(ImageSource), typeof(Country));
+
+
+    }
+}
