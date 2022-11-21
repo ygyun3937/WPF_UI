78501dc4c275208935d32812079ff83c465fa0a7
ygyun3937 <dudrhs5124@naver.com>
2022-11-22 00:00:59 +0900

    2022-11-21 MVVM Init

---
+++ WPF_UI/WPF_UI/Models/NotifierMySql.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.ComponentModel;
+
+namespace WPF_UI.Models
+{
+    class NotifierMySql : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void NotifyChanged(params string[] propertyNames)
+        {
+            foreach (string name in propertyNames)
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs(name));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+}
---
+++ WPF_UI/WPF_UI/ViewModel/DelegateCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace WPF_UI.ViewModel
+{
+    public class DelegateCommand :ICommand
+    {
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        public DelegateCommand(Action execute):this(execute,null)
+        {
+
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        /// <summary>
+        /// can Executes event Handler
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// implement of icommand can execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icommand interface</param>
+        /// <returns>can execute or not</returns>
+        public bool CanExecute(object o)
+        {
+            if(this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        /// <summary>
+        /// implement of icommand interface execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icommand interface</param>
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            if(this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+
+
+    }
+}
